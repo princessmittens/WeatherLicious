e4152fdffcc89bff5370ce481b9a455df0c7ac7f
princessmittens <an747455@dal.ca>
2018-03-19 10:06:55 -0300

    Working on backend

--- app/src/main/java/com/example/achristians/asgn3/MainActivity.java
+++ app/src/main/java/com/example/achristians/asgn3/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.achristians.asgn3;
 
 import android.app.DownloadManager;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -26,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
     EditText weatherTxt;
     String city;
     String apiKey = "&APPID=0533bae34d4a2010f556ce49f3137055";
+    private Runnable runnable;
+    double d = 55.3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,14 +48,23 @@ public class MainActivity extends AppCompatActivity {
         weather.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                city = weatherTxt.getText().toString();
+                city = weatherTxt.getText().toString().toLowerCase();
                 runnable = new Runnable() {
                     @Override
-                    public void run() {
-                        getWeather();
+                public void run() {
+                         getWeather();
                     }
-                };
+            };
+                Log.i("Tag", "mittens");
+
+                Thread thread = new Thread(null, runnable, "background");
+                thread.start();
+
+
+                InputMethodManager inputManager =  (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
             }
 
         });
@@ -58,19 +72,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void getWeather() {
 
-            final String url = "https://api.openweathermap.org/data/2.5/weather?q=";
+            final String url = "http://api.openweathermap.org/data/2.5/forecast?q=";
             String urlWithBase = url.concat(city+apiKey);
 
-            JsonObjectRequest request = new JSONObjectRequest(
+            JsonObjectRequest request = new JsonObjectRequest(
                     Request.Method.GET, urlWithBase, null,
                     new Response.Listener<JSONObject>()
                     {
                         public void onResponse(JSONObject response) {
-                            Toast.makeText(getApplicationContext(), "Here it comes!", Toast.LENGTH_SHORT).show();
-
+                            Toast.makeText(getApplicationContext(),"getting data", Toast.LENGTH_LONG).show();
                             try{
-                                response = response.getJSONObject("main");
-                                String humidity = response.humidity;
+
+                                JSONObject main = response.getJSONObject("main");
+                                String temp = main.getString("temp");
+
+                                setTemp(temp);
+
 
                             } catch(JSONException e){
                                 e.printStackTrace();
@@ -84,5 +101,11 @@ public class MainActivity extends AppCompatActivity {
             }
             );
             RequestQueueSingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+        }
+
+
+        public void setTemp(String temp){
+
+            degrees.setText(temp);
         }
 }
